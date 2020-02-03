01d5c266cb1a99202d69f2ee95685bfed26c15f4
Laura Ghiorghisor <laura.ghiorghisor@gmail.com>
2020-02-03 13:30:41 +0000

    hello

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# GolfSite
+# barclaycard
+Barclaycard group
---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+<?php 
+
+
+$login ='Log in';
+
+
+ob_start();
+require '../templates/topnav.html.php';
+$topnav = ob_get_clean();
+
+
+ob_start();
+require '../templates/catnav.html.php';
+$catnav = ob_get_clean();
+
+
+ob_start();
+require '../templates/carousel.html.php';
+$carousel = ob_get_clean();
+
+
+ob_start();
+require '../templates/home-page-content.html.php';
+$content = ob_get_clean();
+
+
+ob_start();
+require '../templates/footer.html.php';
+$footer = ob_get_clean();
+
+
+require '../templates/layout.html.php';
+?>
