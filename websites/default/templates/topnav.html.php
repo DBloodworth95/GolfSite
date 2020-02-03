4cee9113ac208f720c380e67fcaaa93f74bd0c20
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-02-03 13:52:45 +0000

    Merged project

--- websites/default/templates/topnav.html.php
+++ websites/default/templates/topnav.html.php
@@ -14,7 +14,7 @@
 
             <div class=" px-0 d-flex flex-row justify-content-space-between">
                 <div class="px-0 d-flex flex-row ">
-                    <a class="topbar-link nav-item nav-link" href="basket.html">Account</a>
+                    <a class="topbar-link nav-item nav-link" href="/basket.html">Account</a>
                     <a class="account nav-item nav-link" href="basket.html"></a>
                     <a class="topbar-link nav-item nav-link" href="basket.html">Basket</a>
                     <a class="basket nav-item nav-link" href="basket.html"><span class="basket-counter">100</span></a>
